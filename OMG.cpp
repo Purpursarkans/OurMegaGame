164f1667318e529b9613c530274252c99c0059d8
Purpursarkans <goga.danilov10@gmail.com>
2023-08-30 12:27:20 +0400

    Update

--- OMG.cpp
+++ OMG.cpp
@@ -1,20 +1,24 @@
 #include "HEAD.HPP"
 
+static int WINDOW_WIDTH = 1280;
+static int WINDOW_HEIGHT = 720;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "OMG", sf::Style::Close);
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Magenta);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        
         window.clear();
         window.draw(shape);
         window.display();
